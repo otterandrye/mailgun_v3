7e769caf929d5bb43507bb53029d4d42c7c0207a
Jens Walter <jens@apimeister.com>
2021-11-14 20:18:52 +0100

    add examples

---
+++ examples/send_email.rs
@@ -0,0 +1,20 @@
+use mailgun_v3::email::Message;
+use mailgun_v3::email::EmailAddress;
+use mailgun_v3::email::MessageBody;
+use mailgun_v3::Credentials;
+
+fn main(){
+    let both = Message {
+        to: vec![EmailAddress::address("target@example.org")],
+        body: MessageBody::Text("hello world".to_string()),
+        subject: String::from("sample subject"),
+        ..Default::default()
+    };
+    let sender = EmailAddress::address("sender@example.org");
+    let creds = Credentials::new(
+        "key-abc1234567890",
+        "example.org",
+    );
+    let res = mailgun_v3::email::send_email(&creds, &sender, both);
+    println!("{:?}", res);
+}
